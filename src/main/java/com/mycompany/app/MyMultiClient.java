6d86be7b1366d3e411ed70fc9a77069d2fe5fdf4
Manas Malla <manasmalla.dev@gmail.com>
2024-03-13 04:52:23 +0000

    Added a multi-modal messaging system

---
+++ src/main/java/com/mycompany/app/MyMultiClient.java
@@ -0,0 +1,39 @@
+
+package com.mycompany.app;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+class MyMultiClient{
+    public static void main(String[] args){
+        try{
+            System.out.println("Waiting... ");
+            Socket socket = new Socket("localhost", 56);
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            Scanner scanner= new Scanner(System.in);
+            String response = dis.readUTF();
+            while(!response.contains("bye")){
+                System.out.println("Server: " + response);
+                System.out.print("> ");
+                String request = scanner.nextLine();
+                dos.writeUTF(request);
+                response = dis.readUTF();
+            }
+            dos.writeUTF("bye");
+            System.out.println("Connection closed");
+        }catch(IOException e){
+            System.out.println("I/O Error: " + e.getMessage());
+        }catch(SecurityException e){
+            System.out.println("Security Error: " + e.getMessage());
+        }catch(IllegalArgumentException e){
+            System.out.println("Invalid Argument Error: " + e.getMessage());
+        }catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/mycompany/app/MyMultiServer.java
@@ -0,0 +1,45 @@
+
+package com.mycompany.app;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+class MyMultiServer{
+    public static void main(String[] args){
+        try{
+            System.out.println("Waiting... ");
+            ServerSocket ss = new ServerSocket(56);
+            while(true){
+                Socket socket = ss.accept();
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+           
+            Scanner scanner= new Scanner(System.in);
+            dos.writeUTF("Connection established");
+            String response = (String)dis.readUTF();
+            while(!response.contains("bye")){
+                System.out.println("Client: " + response);
+                System.out.print("> ");
+                String request = scanner.nextLine();
+                dos.writeUTF(request);
+                response = (String)dis.readUTF();
+            }
+            dos.writeUTF("bye");
+            System.out.println("Connection closed");
+            }
+            // ss.close();
+        }catch(IOException e){
+            System.out.println("I/O Error: " + e.getMessage());
+        }catch(SecurityException e){
+            System.out.println("Security Error: " + e.getMessage());
+        }catch(IllegalArgumentException e){
+            System.out.println("Invalid Argument Error: " + e.getMessage());
+        }catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
